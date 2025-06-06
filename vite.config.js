4547bc173374f19d157477b424a226acd0adbbba
Abdul Razaq <razaq23112000@gmail.com>
2025-06-06 12:07:21 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'; // ✅ Import this!
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/', // ✅ Your GitHub repo name
   plugins: [react()],
-});
+  base: '/your-repo-name/', // ⚠️ Important
+})
